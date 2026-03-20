cc06eeb43234c1da9bc43e377daa37e3f24a26f5
nillieyatted <corybilalneal@gmail.com>
2026-03-20 00:48:25 -0400

    Added first Python script

---
+++ first_script.py
@@ -0,0 +1,15 @@
+# My first Python script
+# Cory Neal - Cybersecurity & AI
+# 
+print("===============================")
+print("        CORY NEAL - IT         ")
+print("===============================")
+print("")
+print("Name:    Cory Neal")
+print("Focus:   CyberSecuriry & AI")
+print("Certs:   A+ | Net+ | Sec+ | Azure | Linux")
+print("Goal:    Penetration Tester / AI Engineer")
+print("")
+print("===============================")
+print("      LET THE WORK BEGIN       ")
+print("===============================")
